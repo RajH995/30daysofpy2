184e6a87a5a51326d6a96036030e9a3b9bcb67a2
Rajeev H <ballinglikecurry387@gmail.com>
2026-07-07 10:46:57 -0700

    yes king

---
+++ day8/dih.py
@@ -0,0 +1,20 @@
+dog = {}
+dog['name'] = 'Buddy'
+dog['color'] = 'Brown'
+dog['breed'] = 'Labrador'
+dog['legs'] = 4
+dog['age'] = 5
+
+student = {}
+student['first_name'] = 'John'
+student['last_name'] = 'Doe'
+student['gender'] = 'Male'
+student['age'] = 20
+student['marital_status'] = 'Single'
+student['skills'] = ['Python', 'JavaScript', 'HTML']
+student['country'] = 'USA'
+student['city'] = 'New York'   
+student['address'] = '123 Main St'
+
+first_key = next(iter(student))
+
