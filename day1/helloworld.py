b2f83b862e139b317d9a904a180eb6389a241a5c
Rajeev H <ballinglikecurry387@gmail.com>
2026-07-23 21:58:02 -0700

    problems wereass

--- day1/helloworld.py
+++ day1/helloworld.py
@@ -1,29 +1,81 @@
-# my_list = [1, 2, 3, 4, 5]
+# # my_list = [1, 2, 3, 4, 5]
 
-# for i in range(len(my_list)//2):
-#     my_list[i], my_list[-(i + 1)] = my_list[-(i + 1)], my_list[i]
+# # for i in range(len(my_list)//2):
+# #     my_list[i], my_list[-(i + 1)] = my_list[-(i + 1)], my_list[i]
 
-# print(my_list)  # Output: [5, 4, 3, 2, 1]
+# # print(my_list)  # Output: [5, 4, 3, 2, 1]
 
-def romantoint(roman_num):
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    total = 0
-    prev_value = 0
+# def romantoint(roman_num):
+#     roman_dict = {
+#         'I': 1,
+#         'V': 5,
+#         'X': 10,
+#         'L': 50,
+#         'C': 100,
+#         'D': 500,
+#         'M': 1000
+#     }
+#     total = 0
+#     prev_value = 0
 
-    for char in reversed(roman_num):
-        value = roman_dict[char]
-        if value < prev_value:
-            total -= value
-        else:
-            total += value
-        prev_value = value
+#     for char in reversed(roman_num):
+#         value = roman_dict[char]
+#         if value < prev_value:
+#             total -= value
+#         else:
+#             total += value
+#         prev_value = value
 
-    return total
+#     return total
+
+
+# def letterCombinations(digits):
+#     if not digits:
+#         return []
+
+#     digit_to_letters = {
+#         '2': 'abc',
+#         '3': 'def',
+#         '4': 'ghi',
+#         '5': 'jkl',
+#         '6': 'mno',
+#         '7': 'pqrs',
+#         '8': 'tuv',
+#         '9': 'wxyz'
+#     }
+
+#     def backtrack(index, path):
+#         if index == len(digits):
+#             combinations.append("".join(path))
+#             return
+#         possible_letters = digit_to_letters[digits[index]]
+#         for letter in possible_letters:
+#             path.append(letter)
+#             backtrack(index + 1, path)
+#             path.pop()
+
+#     combinations = []
+#     backtrack(0, [])
+#     return combinations
+
+# def isPalindrome(x):
+#         my_x = str(x)
+#         reversed_x = "".join(reversed(my_x))
+#         for i in range(len(my_x)):
+#             print(my_x[i], reversed_x[i])
+#             if my_x[i] == reversed_x[i]:
+#                 pass
+#             else:
+#                 return False
+#         return True
+
+
+# print(isPalindrome(121))
+
+my_list = [1, 2, 3, 4, 5, 6]
+
+firstHalf = my_list[0:len(my_list)//2]
+secondHalf = my_list[len(my_list)//2::]
+
+print(firstHalf)
+print(secondHalf)
---
+++ day14/higher.py
@@ -0,0 +1,23 @@
+from countries import countries_data
+
+
+countries = ['Estonia', 'Finland', 'Sweden', 'Denmark', 'Norway', 'Iceland']
+names = ['Asabeneh', 'Lidiya', 'Ermias', 'Abraham']
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(list(map(lambda x: x.upper(), countries)))
+
+# def starts_with_E(country):
+#      return country.startswith('E')
+
+# print(list(filter(starts_with_E, countries)))
+
+# def capAnd_filter():
+#     return [c for c in map(lambda x: x.upper(), filter(starts_with_E, countries))]
+
+# print(capAnd_filter())
+
+def catogorize_countries_by_pattern(pattern):
+    return list(filter(lambda country: pattern in country["name"], countries_data))
+
+print(catogorize_countries_by_pattern('land'))
